f4d3937c7cac3013a3ca1c036c93f6f5964279be
HcySunYang <HcySunYang@outlook.com>
2017-11-17 14:43:41 +0800

    feature: `key` is an undefined property on the source object

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const isObject = obj => Object.prototype.toString.call(obj) === '[object Object]'
+const warnUndefined = key => console.error(`\`${key}\` is an undefined property on the source object`)
 
-export default function deepPick (source, partten) {
+export default function deepPick (source, partten, options) {
+  options = options || {}
   if (Array.isArray(partten)) {
     const res = []
     source.forEach((o, i) => {
-      res[i] = deepPick(o, partten[0])
+      res[i] = deepPick(o, partten[0], options)
     })
     return res
   } else if (isObject(partten)) {
@@ -13,7 +15,12 @@ export default function deepPick (source, partten) {
       if (!Array.isArray(partten[key]) && !isObject(partten[key])) {
         res[key] = source[key]
       } else {
-        res[key] = deepPick(source[key], partten[key])
+        if (!Array.isArray(source[key]) && !isObject(source[key])) {
+          options.warn && warnUndefined(key)
+          res[key] = source[key]
+        } else {
+          res[key] = deepPick(source[key], partten[key], options)
+        }
       }
     })
     return res
